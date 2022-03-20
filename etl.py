27d4d06a9444be61802b8fbc091c88e31c7f1aef
Sibusiso-Mgidi <mgiditercy@gmail.com>
2022-03-20 04:58:56 +0200

    added and defined method used to establish connection to the database

--- etl.py
+++ etl.py
@@ -17,4 +17,26 @@ class MoviedDB():
         self.token = data["token"]
         self.database = data["database_name"]
 
+    def get_establish_connection(self):
+        """
+        This method stablish connection to the database.
+        :returns: 
+        -- conn: network connection to the database.
+        -- cur: connectin cursor object.
+        """
+        conn =  pymysql.connect(host = self.host_name, port = self.port_number, user = self.user_name, passwd = self.token, db = self.database)
+        cur = conn.cursor()
 
+        return conn, cur
+
+
+
+with open("config.json") as config_file:
+    # get database login credintials.
+    data = json.load(config_file)
+
+# get an automated database object 
+    moviesdb_object = MoviedDB(data)
+    conn, cur =  moviesdb_object.get_establish_connection()
+    
+    
